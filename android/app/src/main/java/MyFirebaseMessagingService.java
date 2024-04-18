f637ce960c4e4d327015d78b9bf3f2a75a0a817f
deadpearl <adilbayevameruert@gmail.com>
2024-04-18 22:25:52 +0500

    new files

---
+++ android/app/src/main/java/MyFirebaseMessagingService.java
@@ -0,0 +1,8 @@
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+    }
+}
--- android/app/src/main/kotlin/com/example/kazgeowarningmobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kazgeowarningmobile/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.kazgeowarningmobile
 
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Context
+import androidx.multidex.MultiDex
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
+
